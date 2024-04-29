4d073514cd860468b4ccb56c9e70cf1f88d76649
Satcheen Shakya <satcheen@rubiku.be>
2024-04-29 11:48:06 +0200

    feat: setup basic functionality

--- src/store.ts
+++ src/store.ts
@@ -20,33 +20,77 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+
+
+export function Restrict(...params: any[]): any {
+
+  return function (target: any, key: any, descriptor: PropertyDescriptor) {
+    let value = target[key];
+
+    const getter = function () {
+      return value;
+    };
+
+    const setter = function (newVal: any) {
+      value = newVal;
+    };
+
+    if (delete target[key]) {
+      Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+        enumerable: true,
+        configurable: true
+      });
+    }
+  };
 }
 
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
+  private data: any = {};
+
 
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const descriptor = Object.getOwnPropertyDescriptor(this, key);
+    if (descriptor && descriptor.get) {
+      console.log(descriptor.get());
+      return descriptor.get().includes("r");
+    }
+    return this.defaultPolicy.includes("r");
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    // throw new Error("Method not implemented.");
+    const descriptor = Object.getOwnPropertyDescriptor(this, key);
+    if (descriptor && descriptor.get) {
+      return descriptor.get().includes("w");
+    }
+    return this.defaultPolicy.includes("w");
   }
 
   read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
+    // throw new Error("Method not implemented.");
+    return this.data[path];
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
+    // throw new Error("Method not implemented.");
+    this.data[path] = value;
+    return value;
   }
 
   writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
+    // throw new Error("Method not implemented.");
+    for (const key in entries) {
+      if (entries.hasOwnProperty(key)) {
+        this.write(key, entries[key]);
+      }
+    }
   }
 
   entries(): JSONObject {
-    throw new Error("Method not implemented.");
+    // throw new Error("Method not implemented.");
+    return this.data;
   }
 }
