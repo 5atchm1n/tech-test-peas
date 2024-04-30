81c8526e44e04ddb731e472eb5956cf94aa39824
Satcheen Shakya <satcheen@rubiku.be>
2024-04-30 16:52:20 +0200

    feat: add recursive methods to read and write data

--- src/store.ts
+++ src/store.ts
@@ -12,6 +12,10 @@ export type StoreValue =
   | StoreResult
   | (() => StoreResult);
 
+export enum DataType {
+  DEFAULT, STORE, FUNCTION
+}
+
 export interface IStore {
   defaultPolicy: Permission;
   allowedToRead(key: string): boolean;
@@ -24,12 +28,9 @@ export interface IStore {
 
 
 
-export function Restrict(...permission: Permission[]): any {
+export function Restrict(...permissions: Permission[]): any {
   return function (target: IStore, key: string) {
-    if (permission.length === 0) {
-      permission = [target.defaultPolicy];
-    }
-    Reflect.defineMetadata("permissions", permission, target, key);
+    Reflect.defineMetadata("permission", permissions, target, key);
   }
 }
 
@@ -39,28 +40,61 @@ export class Store implements IStore {
 
 
   allowedToRead(key: string): boolean {
-    const permissions: Permission[] = Reflect.getMetadata("permissions", this, key);
+    const permissions: Permission[] = Reflect.getMetadata("permission", this, key);
+    if (!permissions) {
+      return this.defaultPolicy === "r" || this.defaultPolicy === "rw";
+    }
     return permissions.includes("r") || permissions.includes("rw");
   }
 
   allowedToWrite(key: string): boolean {
-    const permissions: Permission[] = Reflect.getMetadata("permissions", this, key);
+    const permissions: Permission[] = Reflect.getMetadata("permission", this, key);
+    if (!permissions) {
+      return this.defaultPolicy === "w" || this.defaultPolicy === "rw";
+    }
     return permissions.includes("w") || permissions.includes("rw");
   }
 
   read(path: string): StoreResult {
-    // throw new Error("Method not implemented.");
-    return this.data[path];
+    const keys = this._splitPath(path);
+    const primaryKey = keys[0];
+    const keyFragment = keys.slice(1).join(":");
+    const data: any = this;
+
+    // check inheritance
+    if (keys.length > 1 && data[primaryKey] instanceof Store) {
+      return data[primaryKey].read(keyFragment);
+    }
+
+    if (!this.allowedToRead(keys[0])) {
+      throw new Error('No permission for read');
+    }
+
+    const result = keys.length > 1 ? this._Read(keys, this) : data[primaryKey]
+
+    return result instanceof Function ? result() : result;
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    // throw new Error("Method not implemented.");
-    this.data[path] = value;
+    const keys = this._splitPath(path);
+    const primaryKey = keys[0];
+    const data: any = this;
+
+    if (keys.length > 1 && data[primaryKey] instanceof Store) {
+      return data[primaryKey].write(keys.slice(1).join(':'), value);
+    }
+
+    if (!this.allowedToWrite(primaryKey)) {
+      throw new Error('No permission for write');
+    }
+
+    this._Write(keys, this, value);
+
     return value;
+
   }
 
   writeEntries(entries: JSONObject): void {
-    // throw new Error("Method not implemented.");
     for (const key in entries) {
       if (entries.hasOwnProperty(key)) {
         this.write(key, entries[key]);
@@ -69,7 +103,82 @@ export class Store implements IStore {
   }
 
   entries(): JSONObject {
-    // throw new Error("Method not implemented.");
-    return this.data;
+    const keys = Object.keys(this);
+    const entries: JSONObject = {};
+    const data: any = this;
+
+    for (const key of keys) {
+      if (Reflect.getMetadata('permission', this, key) && this.allowedToRead(key)) {
+        entries[key] = data[key]
+      }
+    }
+    return entries
+  }
+
+  /**
+   * read and write helper functions
+  **/
+
+  private _Read(keys: string[], data: any): StoreResult {
+    const currentKey = keys[0];
+    const dataType = this._isInstanceOf(data[currentKey]);
+    const nextKeyFragment = keys.slice(1);
+
+    switch (dataType) {
+        case DataType.STORE:
+            return this._Read(nextKeyFragment, data[currentKey]);
+        case DataType.FUNCTION:
+            return this._readFunctionValue(data[currentKey], keys);
+        default:
+            return this._readDataValue(data[currentKey], keys);
+    }
+  }
+
+  private _Write(keys: string[], data: any, value: StoreValue): void {
+    const currentKey = keys[0];
+    const nextKeyFragment = keys.slice(1);
+
+    if (!this._isFinalKey(keys)) {
+      if (data[currentKey] instanceof Store) {
+        this._Write(nextKeyFragment, data[currentKey], value);
+      } else {
+        data[currentKey] = data[currentKey] || {};
+        this._Write(nextKeyFragment, data[currentKey], value);
+      }
+    } else {
+      data[currentKey] = value;
+    }
+  }
+
+  /**
+  * UTILITY FUNCTIONS
+  **/
+
+  private _readFunctionValue(data: Function, keys: string[]) {
+    const nextKeyFragment = keys.slice(1);
+    return this._isFinalKey(keys) ? data(): this._Read(nextKeyFragment, data());
+  }
+
+  private _readDataValue(data: any, keys: string[]) {
+    const nextKeyFragment = keys.slice(1);
+    return this._isFinalKey(keys) ? data: this._Read(nextKeyFragment, data);
+  }
+
+  private _splitPath(path: string): string[] {
+    return path.split(":");
+  }
+
+  private _isFinalKey(keys: string[]): boolean {
+    return keys.length === 1;
+  }
+
+  private _isInstanceOf(data: any): DataType {
+    if (data instanceof Store) {
+      return DataType.STORE;
+    }
+    if (data instanceof Function) {
+      return DataType.FUNCTION;
+    }
+    return DataType.DEFAULT;
   }
 }
