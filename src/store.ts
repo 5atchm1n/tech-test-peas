ca5dab67f8973d7a7934d68f097ea59ffc4dcadb
Satcheen Shakya <satcheen@rubiku.be>
2024-04-30 11:14:18 +0200

    feat: use reflect-metadata package

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { JSONArray, JSONObject, JSONPrimitive } from "./json-types";
+import 'reflect-metadata';
 
 export type Permission = "r" | "w" | "rw" | "none";
 
@@ -20,33 +21,75 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+export function Restrict(...permission: Permission[]) {
+  // write a custom decorator here
+    return function(target: any, key: string) {
+        Reflect.defineMetadata('permission', permission, target, key);
+    }
 }
 
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
 
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const permission: Permission = Reflect.getMetadata('permission', this, key) || this.defaultPolicy;
+    return permission.includes("r") || permission.includes("rw");
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const permission: Permission = Reflect.getMetadata('permission', this, key) || this.defaultPolicy;
+    return permission.includes("w") || permission.includes("rw");
   }
 
   read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
+    const keys = path.split(":");
+    let value: any = this;
+    for (const key of keys) {
+      value = value[key];
+      if (!this.allowedToRead(value)) {
+        throw new Error(`Cannot read property ${key}`);
+      }
+      if (value === undefined) {
+        return undefined;
+      }
+    }
+    return value;
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
+    const keys = path.split(":");
+    let target: any = this;
+    for (let i = 0; i < keys.length - 1; i++) {
+      const key = keys[i];
+      if (!this.allowedToWrite(target[key])) {
+        throw new Error(`Cannot write to property ${key}`);
+      }
+      if (!(target[key] instanceof Object)) {
+        target[key] = {};
+      }
+      target = target[key];
+    }
+    const lastKey = keys[keys.length - 1];
+    if (!this.allowedToWrite(lastKey)) {
+      throw new Error(`Cannot write to property ${lastKey}`);
+    }
+    target[lastKey] = value;
+    return value;
   }
 
   writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
+    for (const key in entries) {
+      this.write(key, entries[key]);
+    }
   }
 
   entries(): JSONObject {
-    throw new Error("Method not implemented.");
+    const result: JSONObject = {};
+    // for (const key in this.data) {
+    //   if (this.allowedToRead(key)) {
+    //     result[key] = this.data[key];
+    //   }
+    // }
+    return result;
   }
 }
